d746fc59bdbf12c379a861ad7efccdcdb047f2b6
KamWyp93 <kamilus9322@user.noreply.github.com>
2021-05-09 11:41:38 +0000

    zadanie 5

---
+++ zadanie5.py
@@ -0,0 +1,13 @@
+marka = "Peugeot"
+ilosc_drzwi = 5
+pojemnosc = 1.3
+
+marka_up = marka.upper()
+
+print ("Samochod " + marka_up)
+print (marka_up)
+print ("Pojemnosc: " + str(pojemnosc))
+
+marka_low = marka.lower()
+
+print ("A z malej to bedzie " + marka_low)
